d7a080e5388b1d007c62a60ff994072b8c8356df
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-02-08 23:37:54 -0500

    pass test 4

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ function pigLatin(input) {
 		if (input[i] !== "a" && input[i] !== "e" && input[i] !== "i" && input[i] !== "o" && input[i] !== "u") {
 			startingConsonants += input[i];
 		}
+		else if (input[i] === "u" && input[i-1] === "q") {
+			startingConsonants += input[i];
+		}
 		else {
 			let pigLatinEndingLetters = "";
 			if (i === 0) {
@@ -20,12 +23,4 @@ function pigLatin(input) {
 	
 }
 
-/*   if (input[0] === "a" || input[0] === "e" || input[0] === "i" || input[0] === "o" || input[0] === "u") {
-    return input.concat("way");
-} else {
-    
-    return input.slice(1).concat(input[0]).concat("ay");
-	}
-}
- */
-console.log(pigLatin("castle"));
+console.log(pigLatin("a"));
