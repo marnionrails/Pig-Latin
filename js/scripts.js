97ca2fbe52460e3fc3c3bb37fde27d19cfa8479e
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-02-08 23:18:40 -0500

    pass test 3
    co-authored-by: Shanen Cross <shanencross@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
 function pigLatin(input) {
+	let startingConsonants = "";
+	for (let i=0; i<input.length; i+=1) {
+		if (input[i] !== "a" && input[i] !== "e" && input[i] !== "i" && input[i] !== "o" && input[i] !== "u") {
+			startingConsonants += input[i];
+		}
+		else {
+			let pigLatinEndingLetters = "";
+			if (i === 0) {
+				pigLatinEndingLetters = "way";
+			}
+			else {
+				pigLatinEndingLetters = "ay";
+			}
 
-  if (input[0] === "a" || input[0] === "e" || input[0] === "i" || input[0] === "o" || input[0] === "u") {
+			return input.slice(i).concat(startingConsonants).concat(pigLatinEndingLetters);
+		}
+	}
+
+	
+}
+
+/*   if (input[0] === "a" || input[0] === "e" || input[0] === "i" || input[0] === "o" || input[0] === "u") {
     return input.concat("way");
 } else {
+    
     return input.slice(1).concat(input[0]).concat("ay");
 	}
 }
-
+ */
 console.log(pigLatin("castle"));
